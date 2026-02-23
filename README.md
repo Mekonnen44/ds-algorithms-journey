35248bbdf6aa8fdc7b2dc21faacf25756e36b594
Mekonnen Tsehaye <Mekonnentsehaye44@gmail.com>
2026-02-23 04:33:37 +0300

    Initial commit: add DSA folder structure and first problems

---
+++ README.md
@@ -0,0 +1,11 @@
+# DSA Journey
+
+This repository contains my daily practice of Data Structures and Algorithms.
+
+Topics covered:
+- Arrays
+- Linked Lists
+- Stacks
+- Queues
+- Trees
+- Graphs
---
+++ arrays/two_sum.py
@@ -0,0 +1,13 @@
+def two_sum(nums, target):
+    hashmap = {}
+
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in hashmap:
+            return [hashmap[diff], i]
+        hashmap[num] = i
+
+    return []
+
+# test
+print(two_sum([2,7,11,15], 9))  # [0,1]
