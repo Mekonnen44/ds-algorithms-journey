506c1529f583e3970970ed6621647471310a1755
Mekonnen Gebrekidan <Mekonnentsehaye44@gmail.com>
2026-02-24 03:10:26 +0300

    Add stack and linked list problems (valid parentheses, reverse list)

---
+++ linked_lists/reverse_linked_list.py
@@ -0,0 +1,16 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverse_list(head):
+    prev = None
+    curr = head
+
+    while curr:
+        next_temp = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next_temp
+
+    return prev
